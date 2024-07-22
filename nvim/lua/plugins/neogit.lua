0ba5909c661ef78772c6a30d1c19ebd320f4e370
vageeshBairy <vageeshbairy1993@gmail.com>
2024-07-22 14:30:59 +0530

    added descriptions to keyboard shortcuts

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -9,16 +9,16 @@ return {
   config = function()
     local neogit = require 'neogit'
     neogit.setup()
-    vim.keymap.set('n', '<leader>gs', neogit.open, { silent = true, noremap = true })
+    vim.keymap.set('n', '<leader>gs', neogit.open, { silent = true, noremap = true, desc = 'Open Neogit' })
 
-    vim.keymap.set('n', '<leader>gc', ':Neogit commit<CR>', { silent = true, noremap = true })
+    vim.keymap.set('n', '<leader>gc', ':Neogit commit<CR>', { silent = true, noremap = true, desc = 'Neogit Commit' })
 
-    vim.keymap.set('n', '<leader>gp', ':Neogit pull<CR>', { silent = true, noremap = true })
+    vim.keymap.set('n', '<leader>gp', ':Neogit pull<CR>', { silent = true, noremap = true, desc = 'Neogit Pull' })
 
-    vim.keymap.set('n', '<leader>gP', ':Neogit push<CR>', { silent = true, noremap = true })
+    vim.keymap.set('n', '<leader>gP', ':Neogit push<CR>', { silent = true, noremap = true, desc = 'Neogit Push' })
 
-    vim.keymap.set('n', '<leader>gb', ':Telescope git_branches<CR>', { silent = true, noremap = true })
+    vim.keymap.set('n', '<leader>gb', ':Telescope git_branches<CR>', { silent = true, noremap = true, desc = 'Git Branches' })
 
-    vim.keymap.set('n', '<leader>gB', ':G blame<CR>', { silent = true, noremap = true })
+    vim.keymap.set('n', '<leader>gB', ':G blame<CR>', { silent = true, noremap = true, desc = 'Git Blame' })
   end,
 }
