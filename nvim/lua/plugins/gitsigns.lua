d362b2a2584ef918644d35e6025b64b64943c651
vageeshBairy <vageeshbairy1993@gmail.com>
2024-08-12 20:23:23 +0530

    edit - nvim gitsigns

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -31,7 +31,7 @@ return {
     current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts = {
       virt_text = true,
-      virt_text_pos = 'overlay', -- 'eol' | 'overlay' | 'right_align'
+      virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
       delay = 1000,
       ignore_whitespace = false,
       virt_text_priority = 100,
